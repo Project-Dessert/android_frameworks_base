b5463d4d65e47ca0b2521dde6ba4d155bc4e7108
SamarV-121 <samarvispute121@pm.me>
2022-09-28 17:01:14 +0000

    base: Add settings to change QS tile shape [1/2]
    Change-Id: If6ad2f69ac1d8ab15c20be9dbe8531a2ecc13aae

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11151,6 +11151,12 @@ public final class Settings {
         public static final String LOCK_SCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
+         * Quicksettings tile shape
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
